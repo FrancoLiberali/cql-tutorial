df6bff2046649dc380e18ee0231d4eae8eb571eb
Franco Liberali <franco.liberali@gmail.com>
2023-08-16 10:20:47 +0200

    add tutorial 2

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 tutorial_1:
 	go run -tags=tutorial_1 .
+
+tutorial_2:
+	go run -tags=tutorial_2 .
---
+++ tutorial_2.go
@@ -0,0 +1,39 @@
+//go:build tutorial_2
+// +build tutorial_2
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/ditrit/badaas-orm-tutorial/conditions"
+	"github.com/ditrit/badaas-orm-tutorial/models"
+	"github.com/ditrit/badaas/orm"
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+)
+
+// Target: get all cities whose name is 'Paris' and its population is greater than 1000000
+func tutorial(db *gorm.DB, shutdowner fx.Shutdowner) {
+	cities, err := orm.NewQuery[models.City](
+		db,
+		conditions.City.NameIs().Eq("Paris"),
+		conditions.City.PopulationIs().Gt(1000000),
+	).Find()
+
+	// SQL executed:
+	// SELECT cities.* FROM cities
+	// WHERE cities.name = "Paris" AND cities.population > 1000000 AND cities.deleted_at IS NULL
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	log.Println("Cities named 'Paris' with a population bigger than 1.000.000 are:")
+	for i, city := range cities {
+		fmt.Printf("\t%v: %+v\n", i+1, city)
+	}
+
+	shutdowner.Shutdown()
+}
