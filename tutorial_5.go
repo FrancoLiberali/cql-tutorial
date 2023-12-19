4be1f570988ca080393ffd3dd7906a6bd4bdaeb2
Franco Liberali <franco.liberali@gmail.com>
2023-12-19 12:08:09 -0300

    Feature/limit and order (#2)
    * agregar al de main
    
    * move tutorial number
    
    * update badaas version
    
    * add new tutorial

--- tutorial_5.go
+++ tutorial_5.go
@@ -14,29 +14,45 @@ import (
 	"gorm.io/gorm"
 )
 
-// Target: get all cities whose name is 'Paris' and that are the capital of their country
+// Target: get all cities whose name is 'Paris' and preload its country
 func tutorial(db *gorm.DB, shutdowner fx.Shutdowner) {
 	cities, err := orm.NewQuery[models.City](
 		db,
 		conditions.City.NameIs().Eq("Paris"),
-		conditions.City.Country(
-			conditions.Country.CapitalIdIs().Dynamic().Eq(conditions.City.ID),
-		),
+		conditions.City.PreloadCountry(),
 	).Find()
 
+	// Equivalent to:
+	// cities, err := orm.NewQuery[models.City](
+	// 	db,
+	// 	conditions.City.NameIs().Eq("Paris"),
+	// 	conditions.City.Country(
+	// 		conditions.Country.Preload(),
+	// 	),
+	// ).Find()
+
 	// SQL executed:
-	// SELECT cities.* FROM cities
-	// INNER JOIN countries Country ON
-	//    Country.id = cities.country_id AND Country.capital_id = cities.id AND Country.deleted_at IS NULL
+	// SELECT cities.*,
+	//    Country.id AS Country__id,Country.created_at AS Country__created_at,Country.updated_at AS Country__updated_at,Country.deleted_at AS Country__deleted_at,Country.name AS Country__name,Country.capital_id AS Country__capital_id
+	// FROM cities
+	// LEFT JOIN countries Country ON
+	//    Country.id = cities.country_id AND Country.deleted_at IS NULL
 	// WHERE cities.name = "Paris" AND cities.deleted_at IS NULL
 
 	if err != nil {
 		log.Panicln(err)
 	}
 
-	log.Println("Cities named 'Paris' that are the capital of their country are:")
+	log.Println("Cities named 'Paris' are:")
 	for i, city := range cities {
-		fmt.Printf("\t%v: %+v\n", i+1, city)
+		fmt.Printf("\t%v: %+v with country: ", i+1, city)
+
+		cityCountry, err := city.GetCountry()
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		fmt.Printf("%+v\n", cityCountry)
 	}
 
 	shutdowner.Shutdown()
