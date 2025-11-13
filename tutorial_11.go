667c73fa1f9ebd27fc26a1732158d331f0ef28b3
FrancoLiberali <franco.liberali@gmail.com>
2025-11-13 18:42:03 -0300

    add select tutorial

--- tutorial_11.go
+++ tutorial_11.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/FrancoLiberali/cql"
@@ -12,16 +13,41 @@ import (
 	"github.com/FrancoLiberali/cql-tutorial/models"
 )
 
-// Target: verify that cql is compile-time safe
-// Will not compile
+type CityAndCountryNames struct {
+	CityName    string
+	CountryName string
+}
+
+// Target: obtain only the city and country name of Paris, France
 func tutorial(db *cql.DB) {
-	_, err := cql.Query[models.City](
-		context.Background(),
-		db,
-		conditions.Country.Name.Is().Eq(cql.String("Paris")),
-	).Find()
+	results, err := cql.Select(
+		cql.Query[models.City](
+			context.Background(),
+			db,
+			conditions.City.Name.Is().Eq(cql.String("Paris")),
+			conditions.City.Country(
+				conditions.Country.Name.Is().Eq(cql.String("France")),
+			),
+		),
+		cql.ValueInto(conditions.City.Name, func(value string, result *CityAndCountryNames) {
+			result.CityName = value
+		}),
+		cql.ValueInto(conditions.Country.Name, func(value string, result *CityAndCountryNames) {
+			result.CountryName = value
+		}),
+	)
+
+	// SQL executed:
+	// SELECT cities.name, Country.name
+	// FROM cities
+	// INNER JOIN countries Country ON
+	//    Country.id = cities.country_id AND Country.name = "France"
+	// WHERE cities.name = "Paris"
 
 	if err != nil {
 		log.Panicln(err)
 	}
+
+	fmt.Println("--------------------------")
+	fmt.Printf("City named 'Paris' in 'France' is: %+v\n", results)
 }
